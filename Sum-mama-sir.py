6bbb199b7cefcac7869ba2aa881f993b15ce4c20
PanarinDan <6641059922@student.chula.ac.th>
2023-12-08 01:10:12 +0700

    proto bitch

--- Sum-mama-sir.py
+++ Sum-mama-sir.py
@@ -1,7 +1,6 @@
 import streamlit
 import pandas
 import openai
-import json
 
 # get the API from sidebar called OpenAI API key
 openai.api_key = streamlit.secrets["openai_api_key"]
@@ -16,32 +15,32 @@ prompt = """Act as an AI summarizer for English news. You will receive url
             for the news you need to summarize and you should summarize it's 
             with a vocabulary that suitable for high school students.
             """
+# set the title
+streamlit.title("Summarizer")
 
-# set the default response
-response = client.chat.completions.create(
+# set uyser input
+user_input = streamlit.text_input("Enter the url for the news you want to summarize:")
+
+# set the button
+if streamlit.button("Summarize"):
+    # pull news data from url
+    news = pandas.read_html(user_input)
+    
+    # push the news to the AI
+    prompt = prompt + "\n" + news[0][0][0]
+
+    # set the messages so far
+    messages_so_far = [
+        {
+            "AI": user,
+            "Me": prompt
+        },]
+    
+    # set the response
+    response = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=messages_so_far)
+    
+    # show response to client
+    streamlit.write(response)
 
-# set the default messages
-messages_so_far = [
-    {
-        "id": "1",
-        "message": prompt,
-    },
-    {
-        "id": "2",
-        "message": response["choices"][0]["text"],
-    },
-]
-
-# pull the data from the url to sent to Chat gpt
-def pull_data(url):
-    # get the data from the url
-    data = pandas.read_csv(url)
-    # get the data from the url
-    data = data.to_dict()
-    # return the data
-    return data
-
-# set the title of the app
-streamlit.title("Sum-mama-sir")
